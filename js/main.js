837412c646b59460f6e8506d530c39ed054283d6
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-09 16:05:45 +0900

    [Add]v-forでタスクを表示

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 const app = Vue.createApp({
   data: () =>({
+    newItem: '',
+    todos: []
+  }),
+  methods: {
+    addItem: function(event) {
+      // console.log('Clicked')
 
-  })
+      if(this.newItem === '') return
+      let todo = {
+        item: this.newItem
+      }
+      this.todos.push(todo)
+      this.newItem = ''
+    }
+  }
 })
-app.mount('app')
+app.mount('#app')
